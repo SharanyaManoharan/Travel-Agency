2ada0e102d4df917daa1089627e9e92781f3e8b5
Sharanya <sharuster@gmail.com>
2018-02-10 23:38:05 +0530

    Completed desktop and mobile header styles.

--- gulp/tasks/watch.js
+++ gulp/tasks/watch.js
@@ -14,17 +14,25 @@ gulp.task('watch',function(){
 	});
 	//calling the gulp keyword, path to watch the 
 	//required file, 
+
+	/*
 	watch('./app/index.html', 
 		 function(){
 	//function here is calling the
 	//above gulp.task of html and start it using gulp.start
 		browserSync.reload();
-	});
+	});*/
+
+	watch("./app/index.html").on('change',browserSync.reload);
 
 	watch('./app/assets/styles/**/*.css',
 		function(){
 			gulp.start('cssInject');
 		});
+
+	watch('./app/assets/scripts/**/*.js', function(){
+		gulp.start('scriptsRefresh');
+	});
 
 });
 
@@ -32,4 +40,8 @@ gulp.task('watch',function(){
 gulp.task('cssInject',['css'] ,function(){
 	return gulp.src('./app/temp/styles/styles.css')
 	.pipe(browserSync.stream());
-})
+});
+
+gulp.task('scriptsRefresh',['scripts'],function(){
+	browserSync.reload();
+});
